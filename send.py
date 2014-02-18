dc419d73abbad30b109125ee5b89fbe2daaa39f3
root <root@rabbit1.(none)>
2014-02-18 02:09:14 +0000

    nice

--- send.py
+++ send.py
@@ -7,13 +7,12 @@ channel = connection.channel()
 
 channel.queue_declare(queue='hello')
 
-
 total = len(sys.argv)
 if total < 2:
-	print "Must have one argument"
+	print "Must have one argument for the message body"
 	sys.exit(1)
-
 body = str(sys.argv[1])
+
 channel.basic_publish(exchange='', routing_key='hello', body=body)
 print " [x] Sent " + body
 connection.close()
